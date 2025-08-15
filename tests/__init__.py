0403ff137a037752c8e86f9234892dd92271074d
mihai <mihail.onuchin.04@mail.ru>
2025-08-15 15:29:04 +0500

    merge?

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,9 +3,9 @@ import unittest
 from pathlib import Path
 from typing import Any, Dict, List
 
-from src.masks import get_mask_account, get_mask_card_number
-from src.processing import filter_by_state, sort_by_date
-from src.widget import get_date, mask_account_card
+from tests import get_mask_account, get_mask_card_number
+from tests import filter_by_state, sort_by_date
+from tests import get_date, mask_account_card
 
 # Добавление пути для импортов
 sys.path.append(str(Path(__file__).parent))
